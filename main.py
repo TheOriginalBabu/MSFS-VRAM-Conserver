dfc5a6a762c4e36f2d4cb8c60bd313700e7d4eaf
TheOriginalBabu <51098394+TheOriginalBabu@users.noreply.github.com>
2024-04-26 12:45:19 -0400

    pull from applicationList.txt

--- main.py
+++ main.py
@@ -9,30 +9,29 @@ import ctypes, sys
 if ctypes.windll.shell32.IsUserAnAdmin() == 0:
     ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
-if ctypes.windll.shell32.IsUserAnAdmin() == 0:
-    print("Please run this script as an administrator")
-    print("Input anything to exit")
-    input()
-    sys.exit()
+#if ctypes.windll.shell32.IsUserAnAdmin() == 0:
+    #print("Please run this script as an administrator")
+    #print("Input anything to exit")
+    #input()
+    #sys.exit()
 
 print("MSFS VRAM conserver by Babu")
 
-#DGPU_pnpID = r"PCI\VEN_10DE&DEV_2206&SUBSYS_38853842&REV_A1\4&1B844C37&0&0008" #3080
-DGPU_pnpID = r"PCI\VEN_10DE&DEV_24A0&SUBSYS_13201462&REV_A1\4&19F98689&0&0008" #3070ti
+DGPU_pnpID = r"PCI\VEN_10DE&DEV_2206&SUBSYS_38853842&REV_A1\4&1B844C37&0&0008" #3080
+#DGPU_pnpID = r"PCI\VEN_10DE&DEV_24A0&SUBSYS_13201462&REV_A1\4&19F98689&0&0008" #3070ti
 
-def flushDGPU():
-    subprocess.run([r"C:\Windows\System32\pnputil.exe", "/disable-device", DGPU_pnpID])
-    time.sleep(3)
-    subprocess.run([r"C:\Windows\System32\pnputil.exe", "/enable-device", DGPU_pnpID])
-    time.sleep(1)
+#load application list from applicationList.txt
+applicationList = []
+with open("applicationList.txt", "r") as file:
+    for line in file:
+        applicationList.append(line.strip())
+print(applicationList)
 
-#flushDGPU()
+def launchApplications(applicationList):
+    for application in applicationList:
+        subprocess.Popen(application)
 
-applicationList = [ 
-r"C:\Apps\Nvidia Inspector\nvidiaProfileInspector.exe", 
-r"C:\Windows\Notepad.exe"
-]
-
-#loop to run all programs in the list
-for application in applicationList:
-    subprocess.Popen(application)
+#subprocess.run([r"C:\Windows\System32\pnputil.exe", "/disable-device", DGPU_pnpID])
+launchApplications(applicationList)
+#time.sleep(2)
+#subprocess.run([r"C:\Windows\System32\pnputil.exe", "/enable-device", DGPU_pnpID])
